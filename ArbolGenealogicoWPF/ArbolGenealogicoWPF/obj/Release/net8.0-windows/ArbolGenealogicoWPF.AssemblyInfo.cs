1407dd505ebade6c2e1d1c58f0d99c5361d36d28
JoseRojas29 <josegabriel291106@gmail.com>
2025-11-03 17:18:00 -0600

    Limpieza de archivos ignorados

--- ArbolGenealogicoWPF/ArbolGenealogicoWPF/obj/Release/net8.0-windows/ArbolGenealogicoWPF.AssemblyInfo.cs
+++ ArbolGenealogicoWPF/ArbolGenealogicoWPF/obj/Release/net8.0-windows/ArbolGenealogicoWPF.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArbolGenealogicoWPF")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cf6f9dfc455a93e57e919f5665649df1b89c85")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArbolGenealogicoWPF")]
